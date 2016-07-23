aaf2f712fc27996bc5e2e992fedaed26e5a2b3e1
benson-w <yuwang15@illinois.edu>
2016-07-23 13:04:20 -0400

    finished main

--- src/god/Main.java
+++ src/god/Main.java
@@ -2,10 +2,19 @@ package god;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main implements ActionListener {
+	
+	private static int k;
+	Cashier cashier1 = new Cashier();
+	Cashier cashier2 = new Cashier();
+	Cashier cashier3 = new Cashier();
 
 	public static void main(String s[]) {
+		
+		//k = 0;
 
 		JFrame frame = new JFrame("Homework 4");
 
@@ -17,15 +26,20 @@ public class Main {
 		JButton create= new JButton();
 		create.setText("Create Cashier");
 		JButton writeFile = new JButton();
-		writeFile.setText("Write to file!");
+		writeFile.setText("Write to file");
+		JButton execute = new JButton();
+		execute.setText("Run!");
 
 		panel.add(label);
 		panel.add(create);
 		panel.add(writeFile);
+		panel.add(execute);
 		
 		writeFile.setLocation(100, 100);
 		create.setLocation(300,300);
-
+		execute.setLocation(500,100);
+		execute.setBackground(Color.red);
+		
 		frame.add(panel);
 		frame.setSize(500, 200);
 		frame.setLocation(100, 100);
@@ -33,4 +47,20 @@ public class Main {
 		frame.setVisible(true);
 
 	}
+	
+	 @Override
+	 public void actionPerformed(ActionEvent e) {
+	        String command = e.getActionCommand();
+
+	        if (command.equals("execute")) {
+	            myMethod();
+	        }
+	    }
+
+	    public void myMethod() {
+	        k++;
+	        cashier1.run();
+	        cashier2.run();
+	        cashier3.run();
+	    }
 }
