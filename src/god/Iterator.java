336cfb491912a6b102b61a292d56f33a682a2ed1
benson-w <yuwang15@illinois.edu>
2016-08-01 15:39:37 -0400

    added a comment

--- src/god/Iterator.java
+++ src/god/Iterator.java
@@ -1,22 +1,24 @@
 package god;
 
+//asdfjasodfjak;lf
+
 public class Iterator {
-	
+
     private Node current;
 
     //constructor
     public Iterator(Node n){
         current = n;
     }
-    
+
     public void setCurrent(Node n){
     	current = n;
     }
-    
+
     public Node getCurrent() {
     	return current;
     }
-    
+
     public void plusPlus() {
     	if (current == null) {
     		return;
@@ -24,8 +26,8 @@ public class Iterator {
     		current = current.getNext();
     	}
     }
-    
-    
+
+
     //returns to the first node with value
     public void minusMinus(){
     	if (current.getPrevious() == null) {
@@ -34,15 +36,15 @@ public class Iterator {
     		current = current.getPrevious();
     	}
     }
-    
-    
+
+
     public Iterator (){
         current = null;
     }
-    
+
     public Iterator(Iterator i) {
         current = i.getCurrent();
     }
-    
+
 
 }
